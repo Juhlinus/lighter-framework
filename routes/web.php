713300042b0f7d443e18e7f2dd190c73b78c151b
Linus Juhlin <juhlinus@gmail.com>
2019-11-06 09:47:17 +0100

    Addded Regular PHP file handler

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 return \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $route) {
     $route->get('/', \App\Http\Controller\HomeController::class);
     $route->get('/product/{product}', \App\Http\Controller\Product\ShowController::class);
+
+    $route->get('{regular_file:.+}', \App\Http\Controller\RegularPhpFileController::class);
 });
