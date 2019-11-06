ee455dabf9fe0c7153db841e00e21f1dbc269f9b
Linus Juhlin <juhlinus@gmail.com>
2019-11-06 15:20:16 +0100

    Update route to the correct Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
 return \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $route) {
-    $route->get('/', \App\Http\Controller\HomeController::class);
-    $route->get('/product/{product}', \App\Http\Controller\Product\ShowController::class);
+    $route->get('/', \App\Http\Controllers\Home\IndexController::class);
+    $route->get('/product/{product}', \App\Http\Controllers\Product\ShowController::class);
 });
